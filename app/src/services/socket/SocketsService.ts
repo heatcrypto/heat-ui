74fe52928c6839bfb228a80cd92d14b18acc7210
Dennis de Klerk <dmdeklerk@gmail.com>
2016-08-18 15:57:41 +0200

    Expose the Socket class

---
+++ app/src/services/socket/SocketsService.ts
@@ -0,0 +1,35 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+
+@Service('sockets')
+@Inject('$q','$timeout','settings')
+class SocketsService {
+  constructor(private $q, private $timeout, private settings) {}
+
+  private sockets = {};
+
+  public getSocket(id?:string): ISocket {
+    id = id || 'default';
+    return this.sockets[id] || (this.sockets[id] = new Socket(this.$q, this.$timeout, this.settings));
+  }
+}
