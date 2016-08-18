7bf7969e2c0672fab3b29fe5564570515076e4dd
Dennis de Klerk <dmdeklerk@gmail.com>
2016-08-18 19:43:41 +0200

    Rename organization

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,6 +1,6 @@
 /*
  * The MIT License (MIT)
- * Copyright (c) 2016 Krypto Fin ry and the FIMK Developers
+ * Copyright (c) 2016 Heat Ledger Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
