49c6278e6ebe9adbbe429d40061c6b20e474dbd1
Dennis de Klerk <dmdeklerk@gmail.com>
2016-08-22 10:49:34 +0200

    Most basic messenger route

---
+++ app/src/components/routes/messenger/messenger.ts
@@ -0,0 +1,39 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+@RouteConfig('/messenger')
+@Component({
+  selector: 'messenger',
+  template: `
+    <div layout="column" flex layout-align="center center">
+      <h1>Hello {{vm.user.account}}</h1>
+      <p>Welcome to Heat Ledger</p>
+    </div>
+  `
+})
+@Inject('$scope','user')
+class MessengerComponent {
+
+  constructor(private $scope: angular.IScope,
+              public user: UserService) {
+  }
+}
