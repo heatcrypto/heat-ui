44770ceaf890f72cdbfbd39e803545afa369aec6
Dennis de Klerk <dmdeklerk@gmail.com>
2016-09-01 13:50:05 +0200

    Component for copying text, includes a button and toast response

---
+++ app/src/components/copy-text.ts
@@ -0,0 +1,46 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+@Component({
+  selector: 'copyText',
+  inputs: ['@elementId','@message'],
+  styles: [`
+    copy-text .clickable-icon {
+      cursor: pointer;
+    }
+  `],
+  template: `
+    <md-icon md-font-library="material-icons" ng-click="vm.copy(vm.elementId, vm.message)" class="clickable-icon">content_copy</md-icon>
+  `
+})
+@Inject('clipboard')
+class CopyText {
+
+  private elementId: string;
+  private message: string;
+
+  constructor(private clipboard: ClipboardService) {}
+
+  copy(element: string, successMsg: string) {
+    this.clipboard.copyWithUI(document.getElementById(element), successMsg);
+  }
+}
