3a4b63de2a9f0a8cba417d92a905a7cf73c03397
Dennis de Klerk <dmdeklerk@gmail.com>
2016-09-11 16:50:08 +0200

    First iteration of contacts lists in sidebar

---
+++ app/src/components/user-contacts.ts
@@ -0,0 +1,80 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Krypto Fin ry and the FIMK Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+@Component({
+  selector: 'userContacts',
+  styles: [`
+    user-contacts .small-icon {
+      font-size: 24px;
+    }
+  `],
+  template: `
+    <div layout="column" flex>
+      <md-content layout="column">
+        <md-menu-item ng-repeat="contact in vm.contacts" ng-click="vm.goToContact(contact, $event)">
+          <md-button href="#/home" ng-click="vm.close()">
+            <md-icon md-font-library="material-icons" class="md-avatar-icon">person</md-icon>&nbsp;{{contact.account}}
+          </md-button>
+        </md-menu-item>
+      </md-content>
+    </div>
+  `
+})
+@Inject('$scope','user','engine','cloud','$q','$timeout')
+class UserContactsComponent {
+
+  public contacts : Array<ICloudMessageContact> = [];
+
+  constructor(private $scope: angular.IScope,
+              public user: UserService,
+              private engine: EngineService,
+              private cloud: CloudService,
+              private $q: angular.IQService,
+              private $timeout: angular.ITimeoutService) {
+    if (user.unlocked) {
+      this.getContacts();
+    }
+    else {
+      user.on(UserService.EVENT_UNLOCKED, () => { this.getContacts() });
+    }
+  }
+
+  goToContact(contact, $event) {
+
+  }
+
+  getCloudGetMessageContactsRequest() : ICloudGetMessageContactsRequest {
+    return {
+      accountRS: this.user.accountRS,
+      firstIndex: 0,
+      lastIndex: 100
+    }
+  }
+
+  getContacts() {
+    this.cloud.api.getMessageContacts(this.getCloudGetMessageContactsRequest()).then((contacts) => {
+      this.$scope.$evalAsync(() => {
+        this.contacts = contacts
+      });
+    })
+  }
+}
