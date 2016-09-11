270706e67ff9c2777eb9e822fe661ace04ee853d
Dennis de Klerk <dmdeklerk@gmail.com>
2016-09-11 13:12:43 +0200

    Stop opening the dev tools at start of electron app

--- app/electron.js
+++ app/electron.js
@@ -32,7 +32,7 @@ let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
