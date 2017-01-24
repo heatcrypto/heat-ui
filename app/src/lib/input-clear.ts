2408f1f89349644dba664f7bd15b6e131dd53a73
Dennis de Klerk <dmdeklerk@gmail.com>
2017-01-24 14:04:33 +0100

    heat-ui 1.0.0

--- app/src/lib/input-clear.ts
+++ app/src/lib/input-clear.ts
@@ -5,7 +5,7 @@ heat.Loader.directive('inputClear', () => {
       var color = attrs.inputClear;
       var action = attrs.ngModel + " = ''";
       element.after(
-          '<md-button class="animate-show md-icon-button md-accent"' +
+          '<md-button aria-label="Close" class="animate-show md-icon-button md-accent"' +
           'ng-show="' + attrs.ngModel + '" ng-click="' + action + '"' +
           'style="position: absolute; top: 0px; right: -2px; margin-right:0px;">' +
           '<md-icon style="color:black;font-size: 18px;" md-font-library="material-icons">close</md-icon>' +
