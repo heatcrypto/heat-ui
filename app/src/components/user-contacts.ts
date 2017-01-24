f1751a25c5b6e0259501eed6dcaa513f76f99337
Dennis de Klerk <dmdeklerk@gmail.com>
2017-01-24 14:01:49 +0100

    heat-ui 1.0.0

--- app/src/components/user-contacts.ts
+++ app/src/components/user-contacts.ts
@@ -23,54 +23,64 @@
 @Component({
   selector: 'userContacts',
   styles: [`
-    user-contacts .small-icon {
-      font-size: 24px;
+    user-contacts md-list-item {
+      height: 42px !important;
+      padding-top
     }
-    user-contacts md-menu-item.active {
-      font-weight: bold;
-      color: green;
+    user-contacts md-list-item .md-button {
+      padding-left: 0px !important;
     }
-    user-contacts .has-unread-message {
+    user-contacts .md-list-item-inner md-icon {
+      margin-left: 0px !important;
+      margin-right: 16px !important;
+    }
+    user-contacts md-list-item .contact-name {
+      height: 45px !important;
+    }
+    user-contacts .md-secondary-container {
+      margin-top: 5px !important
+    }
+    user-contacts .md-secondary-container .has-unread-message {
       font-size: 100%;
       text-align: center;
       line-height: 1.6;
+    }
+    user-contacts .active-contact {
+      font-weight: bold !important;
     }
   `],
   template: `
     <div layout="column" flex>
       <md-content layout="column">
-        <md-menu-item ng-repeat="contact in vm.contacts"
-          ng-class="{'active': (contact.accountPublicKey == vm.activePublicKey) }">
-          <md-button href="#/messenger/{{contact.accountPublicKey}}">
+        <md-list flex>
+          <md-list-item ng-repeat="contact in vm.contacts" href="#/messenger/{{contact.publicKey}}">
             <md-icon md-font-library="material-icons" class="md-avatar">contact_mail</md-icon>
-            {{contact.account}}
-          </md-button>
-          <md-button href="#/messenger/{{contact.accountPublicKey}}" class="md-secondary md-icon-button md-primary" ng-show="contact.hasUnreadMessage">
-            <md-icon md-font-library="material-icons" class="has-unread-message">fiber_manual_record</md-icon>
-          </md-button>
-        </md-menu-item>
+            <span class="contact-name" ng-class="{'active-contact':(contact.publicKey==vm.activePublicKey)}">{{contact.publicName || contact.account}}</span>
+            <md-icon md-font-library="material-icons" class="md-secondary has-unread-message md-primary" ng-show="contact.hasUnreadMessage">fiber_manual_record</md-icon>
+          </md-list-item>
+        </md-list>
       </md-content>
     </div>
   `
 })
-@Inject('$scope','user','engine','cloud','$q','$timeout','$location','$rootScope','storage')
+@Inject('$scope','user','heat','$q','$timeout','$location','$rootScope','storage','HTTPNotify')
 class UserContactsComponent {
 
-  public contacts : Array<ICloudMessageContact> = [];
+  public contacts : Array<IHeatMessageContact> = [];
   private refresh: IEventListenerFunction;
   private activePublicKey: string;
   private store: Store;
 
   constructor(private $scope: angular.IScope,
               public user: UserService,
-              private engine: EngineService,
-              private cloud: CloudService,
+              private heat: HeatService,
               private $q: angular.IQService,
               private $timeout: angular.ITimeoutService,
               private $location: angular.ILocationService,
               private $rootScope: angular.IRootScopeService,
-              storage: StorageService) {
-
+              storage: StorageService,
+              private HTTPNotify: HTTPNotifyService) {
+    HTTPNotify.on(()=>{this.refresh()}, $scope);
     this.refresh = utils.debounce(
       () => {
         this.getContacts()
@@ -88,42 +98,51 @@ class UserContactsComponent {
       });
     }
 
-    $rootScope.$on('$locationChangeSuccess', () => {
-      var path = $location.path().replace(/^\//,'').split('/'), route = path[0], params = path.slice(1);
-      this.activePublicKey = (route == "messenger") ? params[0] : null;
-    });
+    $rootScope.$on('$locationChangeSuccess', () => { this.setActivePublicKey() });
+    this.setActivePublicKey();
+  }
+
+  getActivePublicKey() {
+    var path = this.$location.path().replace(/^\//,'').split('/'), route = path[0], params = path.slice(1);
+    return (route == "messenger") ? params[0] : null;
+  }
+
+  setActivePublicKey() {
+    this.activePublicKey = this.getActivePublicKey();
+    if (!this.activePublicKey || this.activePublicKey == "0") {
+      if (this.contacts[0] && this.contacts[0].publicKey != "0") {
+        this.$location.path(`/messenger/${this.contacts[0].publicKey}`);
+      }
+    }
   }
 
   init() {
     this.getContacts();
-    var topic = new TransactionTopicBuilder().account(this.user.account);
-    var observer = this.engine.socket().observe<TransactionObserver>(topic).
-      add(this.refresh).
-      remove(this.refresh).
-      confirm(this.refresh);
-    this.$scope.$on("$destroy",() => { observer.destroy() });
-  }
-
-  getCloudGetMessageContactsRequest() : ICloudGetMessageContactsRequest {
-    return {
-      accountRS: this.user.accountRS,
-      firstIndex: 0,
-      lastIndex: 100
-    }
+    // var topic = new TransactionTopicBuilder().account(this.user.account);
+    // var observer = this.engine.socket().observe<TransactionObserver>(topic).
+    //   add(this.refresh).
+    //   remove(this.refresh).
+    //   confirm(this.refresh);
+    // this.$scope.$on("$destroy",() => { observer.destroy() });
   }
 
   getContacts() {
-    this.cloud.api.getMessageContacts(this.getCloudGetMessageContactsRequest()).then((contacts) => {
+    this.heat.api.getMessagingContacts(this.user.account, 0, 100).then((contacts) => {
       this.$scope.$evalAsync(() => {
-        this.contacts = contacts.map((contact) => {
-          contact['hasUnreadMessage'] = this.contactHasUnreadMessage(contact);
+        this.contacts = contacts.filter((contact)=> {
+          return contact.account != this.user.account;
+        }).map((contact) => {
+          contact['hasUnreadMessage'] = true;
           return contact;
-        })
+        });
+        if (!this.getActivePublicKey() || this.getActivePublicKey()=="0") {
+          this.setActivePublicKey();
+        }
       });
     })
   }
 
-  contactHasUnreadMessage(contact: ICloudMessageContact): boolean {
-    return contact.latestTimestamp > this.store.getNumber(contact.account, 0);
+  contactHasUnreadMessage(contact: IHeatMessageContact): boolean {
+    return contact.timestamp > this.store.getNumber(contact.account, 0);
   }
 }
