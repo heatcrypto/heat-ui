3c7bd0d831d771277ceb550ba7548873d4edc29f
Dennis de Klerk <dmdeklerk@gmail.com>
2017-01-24 14:19:36 +0100

    heat-ui 1.0.0

---
+++ app/src/services/transactions/CancelBidOrderService.ts
@@ -0,0 +1,84 @@
+///<reference path='lib/AbstractTransaction.ts'/>
+///<reference path='lib/GenericDialog.ts'/>
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+@Service('cancelBidOrder')
+@Inject('$q','user')
+class CancelBidOrderService extends AbstractTransaction {
+
+  constructor(private $q: angular.IQService,
+              private user: UserService) {
+    super();
+  }
+
+  dialog(order: string, readonly?: boolean, $event?): IGenericDialog {
+    return new CancelBidOrderDialog($event, this, this.$q, this.user, order, readonly);
+  }
+
+  verify(transaction: any, bytes: IByteArrayWithPosition, data: IHeatCreateTransactionInput): boolean {
+    if (transaction.type !== 2) return false;
+    if (transaction.subtype !== 6) return false;
+
+    transaction.order = String(converters.byteArrayToBigInteger(bytes.byteArray, bytes.pos));
+    bytes.pos += 8;
+
+    return transaction.order === data.BidOrderCancellation.orderId;
+  }
+}
+
+class CancelBidOrderDialog extends GenericDialog {
+
+  constructor($event,
+              private transaction: AbstractTransaction,
+              private $q: angular.IQService,
+              private user: UserService,
+              private order: string,
+              private readonly: boolean) {
+    super($event);
+    this.dialogTitle = 'Cancel bid order';
+    this.dialogDescription = 'Description on how to cancel bid order';
+    this.okBtnTitle = 'SEND';
+  }
+
+  /* @override */
+  getFields($scope: angular.IScope) {
+    var builder = new DialogFieldBuilder($scope);
+    return [
+      builder.text('order', this.order).
+              label('Order').
+              required().
+              readonly(this.readonly)
+    ]
+  }
+
+  /* @override */
+  getTransactionBuilder(): TransactionBuilder {
+    var builder = new TransactionBuilder(this.transaction);
+    builder.secretPhrase(this.user.secretPhrase)
+           .feeNQT(HeatAPI.fee.standard)
+           .attachment('BidOrderCancellation', <IHeatCreateBidOrderCancellation>{
+              orderId: this.fields['order'].value
+            });
+    return builder;
+  }
+}
