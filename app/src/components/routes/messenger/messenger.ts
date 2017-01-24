b65949a729eda6a70701f3a54d1cc956ba1836e9
Dennis de Klerk <dmdeklerk@gmail.com>
2017-01-24 14:03:12 +0100

    heat-ui 1.0.0

--- app/src/components/routes/messenger/messenger.ts
+++ app/src/components/routes/messenger/messenger.ts
@@ -25,6 +25,10 @@
   selector: 'messenger',
   inputs: ['publickey'],
   styles: [`
+    messenger user-contacts {
+      width: 240px;
+      min-width: 240px;
+    }
     messenger edit-message {
       min-height: 80px;
     }
@@ -48,21 +52,34 @@
     }
   `],
   template: `
-    <div layout="column" flex layout-padding class="outer-container">
-      <div class="row" class="progress-indicator" flex ng-show="vm.loading">
-        <md-progress-linear class="md-primary" md-mode="indeterminate"></md-progress-linear>
-      </div>
-      <md-content flex id="message-batch-container">
-        <message-batch-viewer flex layout="column" container-id="message-batch-container"
-                publickey="::vm.publickey"></message-batch-viewer>
-      </md-content>
-      <div layout="column" flex="none" class="edit-message">
-        <edit-message publickey="vm.publickey" layout="row" flex></edit-message>
+    <div layout="column" flex layout-padding layout-fill class="outer-container">
+      <div layout="row" flex layout-fill>
+        <div layout="column">
+          <user-contacts flex layout="column" ></user-contacts>
+          <md-button class="md-fab md-primary" aria-label="Add contact" ng-click="vm.showSendmessageDialog($event)">
+            <md-tooltip md-direction="top">
+              Send message to new contact
+            </md-tooltip>
+            <md-icon md-font-library="material-icons">add_circle_outline</md-icon>
+          </md-button>
+        </div>
+        <div layout="column" layout-fill>
+          <div class="row" class="progress-indicator" flex ng-show="vm.loading">
+            <md-progress-linear class="md-primary" md-mode="indeterminate"></md-progress-linear>
+          </div>
+          <md-content flex id="message-batch-container">
+            <message-batch-viewer flex layout="column" container-id="message-batch-container"
+                    publickey="::vm.publickey"></message-batch-viewer>
+          </md-content>
+          <div layout="column" flex="none" class="edit-message">
+            <edit-message publickey="vm.publickey" layout="row" flex></edit-message>
+          </div>
+        </div>
       </div>
     </div>
   `
 })
-@Inject('$scope','user','engine','cloud')
+@Inject('$scope','user','sendmessage')
 class MessengerComponent {
 
   publickey: string; // @input
@@ -70,8 +87,11 @@ class MessengerComponent {
 
   constructor(private $scope: angular.IScope,
               private user: UserService,
-              private engine: EngineService,
-              private cloud: CloudService) {
+              private sendmessage: SendmessageService) {
     user.requireLogin();
+  }
+
+  showSendmessageDialog($event) {
+    this.sendmessage.dialog($event).show();
   }
 }
