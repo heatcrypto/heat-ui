fef2c630401fd117d6f8580e27bdc689be6a8555
Dennis de Klerk <dmdeklerk@gmail.com>
2017-01-31 10:26:56 +0100

    Mark and sort certified markets

--- app/src/components/routes/trader/trader-markets.ts
+++ app/src/components/routes/trader/trader-markets.ts
@@ -63,7 +63,11 @@
         <md-virtual-repeat-container flex layout-fill layout="column"
             virtual-repeat-flex-helper ng-if="vm.markets.length>0">
           <md-list-item md-virtual-repeat="item in vm.markets">
-            <div class="truncate-col market-col"><a href="#/trader/{{item.currency}}/{{item.asset}}">{{item.market}}</a></div>
+            <div class="truncate-col market-col">
+              <a href="#/trader/{{item.currency}}/{{item.asset}}">
+                <span ng-class="{certified:item.currencyInfo.certified}">{{item.currencyInfo.symbol}}</span>/<span ng-class="{certified:item.assetInfo.certified}">{{item.assetInfo.symbol}}</span>
+              </a>
+            </div>
             <div class="truncate-col change-col">{{item.change}}</div>
             <div class="truncate-col price-col">{{item.price}}</div>
             <div class="truncate-col vol-col right-align" flex>{{item.vol}}</div>
@@ -73,7 +77,7 @@
     </div>
   `
 })
-@Inject('$scope','heat','assetInfo','HTTPNotify','storage')
+@Inject('$scope','heat','assetInfo','HTTPNotify','storage','$q')
 class TraderMarketsComponent {
 
   // change, volume, price, none
@@ -86,7 +90,8 @@ class TraderMarketsComponent {
               private heat: HeatService,
               private assetInfo: AssetInfoService,
               HTTPNotify: HTTPNotifyService,
-              private storage: StorageService) {
+              private storage: StorageService,
+              private $q: angular.IQService) {
     HTTPNotify.on(()=>{this.loadMarkets()}, $scope);
     this.loadMarkets();
   }
@@ -95,19 +100,46 @@ class TraderMarketsComponent {
     this.heat.api.getMarketsAll(this.sort, this.asc, "0", 1, 0, 100).then((markets) => {
       this.$scope.$evalAsync(() => {
         this.markets = markets;
+        var promises = []; // collects all balance lookup promises
         this.markets.forEach((market: IHeatMarket|any) => {
-          var currencyInfo = this.assetInfo.parseProperties(market.currencyProperties, {
-            name: "",
-            symbol: market.currency == "0" ? "HEAT" : market.currency
-          });
-          var assetInfo = this.assetInfo.parseProperties(market.assetProperties, {
-            name: "",
-            symbol: market.asset == "0" ? "HEAT" : market.asset
-          });
-          market.market = `${currencyInfo.symbol}/${assetInfo.symbol}`;
+          promises.push(
+            this.assetInfo.getInfo(market.currency).then((info)=>{
+              this.$scope.$evalAsync(() => {
+                market.currencyInfo = {
+                  symbol:info.symbol,
+                  name:info.name,
+                  certified:info.certified
+                };
+              });
+            })
+          );
+          promises.push(
+            this.assetInfo.getInfo(market.asset).then((info)=>{
+              this.$scope.$evalAsync(() => {
+                market.assetInfo = {
+                  symbol:info.symbol,
+                  name:info.name,
+                  certified:info.certified
+                };
+              });
+            })
+          );
           market.change = `${market.hr24Change}%`;
           market.price = utils.formatQNT(market.lastPrice, market.currencyDecimals);
           market.vol = utils.formatQNT(market.hr24AssetVolume, market.assetDecimals);
+          market.currencyInfo = {symbol:'*'};
+          market.assetInfo = {symbol:'*'};
+        });
+        this.$q.all(promises).then(()=>{
+          this.$scope.$evalAsync(() => {
+            this.markets.sort((a:any,b:any)=> {
+              if (a.certified < b.certified) return 1;
+              if (a.certified > b.certified) return -1;
+              if (a.symbol < b.symbol) return 1;
+              if (a.symbol > b.symbol) return -1;
+              return 0;
+            });
+          })
         });
 
         /* PATCHUP IN AWAITING OF SERVER FUNCTIONALITY - also cleanup toolbar.ts */
@@ -123,12 +155,13 @@ class TraderMarketsComponent {
 
             if (m.currency && m.asset) {
               this.markets.push(<any>{
-                market: m.currency.symbol+'/'+m.asset.symbol,
                 currency: m.currency.id,
                 asset: m.asset.id,
                 change: '*',
                 price: '*',
-                vol: '*'
+                vol: '*',
+                currencyInfo: {symbol:m.currency.symbol},
+                assetInfo: {symbol:m.asset.symbol}
               })
             }
           });
