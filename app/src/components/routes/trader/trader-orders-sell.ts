a259c81c2ac237a3a57483a2e4a22eea94dd5e09
Dennis de Klerk <dmdeklerk@gmail.com>
2017-03-17 16:22:01 +0100

    Identify as currently not valid

--- app/src/components/routes/trader/trader-orders-sell.ts
+++ app/src/components/routes/trader/trader-orders-sell.ts
@@ -50,7 +50,8 @@
         </md-list-item>
         <md-virtual-repeat-container md-top-index="vm.topIndex" flex layout-fill layout="column" virtual-repeat-flex-helper>
           <md-list-item md-virtual-repeat="item in vm" md-on-demand
-               ng-click="vm.select(item)" aria-label="Entry">
+               ng-click="vm.select(item)" aria-label="Entry"
+               ng-class="{'virtual': item.unconfirmed, 'currentlyNotValid': item.currentlyNotValid}">
             <div class="truncate-col price-col">{{item.priceDisplay}}</div>
             <div class="truncate-col quantity-col">{{item.quantityDisplay}}</div>
             <div class="truncate-col total-col">{{item.total}}</div>
