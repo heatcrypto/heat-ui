a82d65ae559e6e6e2687588e315f887808663459
Dennis de Klerk <dmdeklerk@gmail.com>
2017-06-17 05:01:22 +0200

    Dont show show/hide sidebar button

--- app/src/components/routes/trader/trader.ts
+++ app/src/components/routes/trader/trader.ts
@@ -26,13 +26,16 @@ declare var Big: any;
   selector: 'trader',
   inputs: ['currency','asset'],
   template: `
-    <div class="balances-row">
+    <div layout="row">
+      <!--
       <div>
         <md-button class="md-icon-button show-hide" aria-label="Show/hide markets" ng-click="vm.toggleMarkets()">
           <md-tooltip md-direction="bottom">Show/Hide markets</md-tooltip>
           <i><img src="assets/{{vm.marketsSidenavOpen?'minusIcon':'plusIcon'}}.png"></i>
         </md-button>
       </div>
+      -->
+      <span flex></span>
       <trader-volume class="trader-component" currency-info="vm.currencyInfo" asset-info="vm.assetInfo" layout="column"></trader-volume>
     </div>
     <div layout="row" flex layout-fill>
