416201992b3f6a36bacfebf5f6e42edc5685e5ab
Dennis de Klerk <dmdeklerk@gmail.com>
2017-06-29 11:13:44 +0200

    Conflicting ts declare

--- app/src/components/routes/new.ts
+++ app/src/components/routes/new.ts
@@ -20,7 +20,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  * */
-declare var __dirname: any;
 @RouteConfig('/new')
 @Component({
   selector: 'new',
