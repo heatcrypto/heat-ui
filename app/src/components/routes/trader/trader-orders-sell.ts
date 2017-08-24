907ccc757e81ec24b943f7e4585c1f65ca8fafca
Theo Robbertze <theo.robbertze@gmail.com>
2017-08-24 14:22:25 +0200

    Account reference popover

--- app/src/components/routes/trader/trader-orders-sell.ts
+++ app/src/components/routes/trader/trader-orders-sell.ts
@@ -25,6 +25,12 @@
   selector: 'traderOrdersSell',
   inputs: ['currencyInfo','assetInfo','selectedOrder'],
   template: `
+    <script type="text/ng-template" id="templateId.tml">
+      <div class="account-popover">
+        <div class="accountId">Account id: {{item.account}}</div>
+        <a href="#/explorer-account/{{item.account}}">Go to account</a>
+      </div>
+    </script>
     <div layout="column" flex layout-fill>
       <div layout="row" class="trader-component-title">Sell {{vm.assetInfo.symbol}}&nbsp;
         <span flex></span>
@@ -33,6 +39,7 @@
       </div>
       <md-list flex layout-fill layout="column" ng-if="vm.currencyInfo&&vm.assetInfo">
         <md-list-item  class="header">
+          <div class="truncate-col info-col"></div>
           <div class="truncate-col price-col">Price</div>
           <div class="truncate-col quantity-col">Quantity</div>
           <div class="truncate-col total-col">Total</div>
@@ -42,6 +49,11 @@
           <md-list-item md-virtual-repeat="item in vm" md-on-demand
                ng-click="vm.select(item)" aria-label="Entry"
                ng-class="{'virtual': item.unconfirmed, 'currentlyNotValid': item.currentlyNotValid||item.cancelled}">
+           <div class="truncate-col info-col" style="z-index:2;">
+             <div class="info" angular-popover direction="right" template-url="templateId.tml" mode="mouseover" style="position: fixed;">
+             </div>
+             <img src="assets/info.png">
+           </div>
             <div class="truncate-col price-col">{{item.priceDisplay}}</div>
             <div class="truncate-col quantity-col">{{item.quantityDisplay}}</div>
             <div class="truncate-col total-col">{{item.total}}</div>
