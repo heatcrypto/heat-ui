b04299c658b8ddb57f83af2dd96386dd804b08cf
resultingforce <alexey.medvedev@gmail.com>
2018-01-09 18:51:56 +0300

    Fix orders running total calculation after first request (download orders in batches)

--- app/src/components/routes/trader/trader-orders-sell.ts
+++ app/src/components/routes/trader/trader-orders-sell.ts
@@ -109,14 +109,12 @@ class TraderOrdersSellComponent extends VirtualRepeatComponent  {
 
           /* preprocessor */
           (firstIndex: number, lastIndex: number, items: Array<IHeatOrder>) => {
-            if (firstIndex == 0) {
-              items.forEach((order)=> {
-                if (order['runningTotalQNT']) {
-                  order['sum'] = utils.formatQNT(order['sumQNT'].toString(), this.currencyInfo.decimals);
-                  order['runningTotal'] = utils.formatQNT(order['runningTotalQNT'].toString(), this.assetInfo.decimals);
-                }
-              });
-            }
+            items.forEach((order) => {
+              if (order['runningTotalQNT']) {
+                order['sum'] = utils.formatQNT(order['sumQNT'].toString(), this.currencyInfo.decimals);
+                order['runningTotal'] = utils.formatQNT(order['runningTotalQNT'].toString(), this.assetInfo.decimals);
+              }
+            });
           }
         );
 
