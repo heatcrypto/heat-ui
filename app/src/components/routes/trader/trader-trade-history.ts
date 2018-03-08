db53804b5e1a942c0823e05b13601bab5c137fa0
Dennis de Klerk <dmdeklerk@gmail.com>
2018-03-08 14:13:34 +0100

    Correct Sell/ Buy label in trader

--- app/src/components/routes/trader/trader-trade-history.ts
+++ app/src/components/routes/trader/trader-trade-history.ts
@@ -140,7 +140,12 @@ class TraderTradeHistoryComponent extends VirtualRepeatComponent  {
       (trade: IHeatTrade|any) => {
         var date = utils.timestampToDate(trade.timestamp);
         trade.time = dateFormat(date, format);
-        trade.type = trade.isBuy ? 'Buy' : 'Sell';
+        if (account) {
+          trade.type = trade.seller == account ? 'Sell' : 'Buy'
+        }
+        else {
+          trade.type = trade.isBuy ? 'Buy' : 'Sell';
+        }
         trade.priceDisplay = utils.formatQNT(trade.price, this.currencyInfo.decimals);
         trade.quantityDisplay = utils.formatQNT(trade.quantity, this.assetInfo.decimals);
         var totalQNT = utils.calculateTotalOrderPriceQNT(trade.quantity, trade.price);
