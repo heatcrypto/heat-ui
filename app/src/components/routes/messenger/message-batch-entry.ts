f9d885a1e86e6a36029d2ba8531b8a42b8e44a7f
Dennis de Klerk <dmdeklerk@gmail.com>
2018-04-03 14:12:54 +0200

    Wrap long messages in messenger, no horizontal scroll

--- app/src/components/routes/messenger/message-batch-entry.ts
+++ app/src/components/routes/messenger/message-batch-entry.ts
@@ -34,8 +34,17 @@
     message-batch-entry .batch-entry {
       padding-left: 0px;
     }
-    message-batch-entry .message-content {
-      word-break: break-all;
+    message-batch-entry .message-content pre {
+      white-space: pre-wrap;       /* Since CSS 2.1 */
+      white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+      white-space: -pre-wrap;      /* Opera 4-6 */
+      white-space: -o-pre-wrap;    /* Opera 7 */
+      word-wrap: break-word;       /* Internet Explorer 5.5+ */
+      /* Adds a hyphen where the word breaks, if supported (No Blink) */
+      -ms-hyphens: auto;
+      -moz-hyphens: auto;
+      -webkit-hyphens: auto;
+      hyphens: auto;
     }
   `],
   template: `
