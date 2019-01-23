fe371ecf5ce42cdab97671916c83c8745cf19969
Abhiram Hatolkar <ahatolkar@grepruby.com>
2019-01-23 14:00:11 +0530

    using old way to show btc tx grid

--- app/src/components/virtual-repeat-btc-transactions.ts
+++ app/src/components/virtual-repeat-btc-transactions.ts
@@ -9,71 +9,54 @@
       </div>
       <md-list flex layout-fill layout="column">
         <md-list-item class="header">
-
           <!-- DATE -->
           <div class="truncate-col date-col left">Time</div>
-
           <!-- TX ID  -->
           <div class="truncate-col tx-col left">Transaction ID</div>
-
           <!-- FROM -->
           <div class="truncate-col info-col left">FROM</div>
-
           <!-- TO -->
           <div class="truncate-col info-col left">TO</div>
-
           <!-- AMOUNT -->
           <div class="truncate-col amount-col left">Amount</div>
-
           <!-- MESSAGE -->
           <div class="truncate-col message-col left">Message</div>
-
           <!-- JSON -->
           <div class="truncate-col json-col"></div>
-
         </md-list-item>
         <md-virtual-repeat-container md-top-index="vm.topIndex" flex layout-fill layout="column" virtual-repeat-flex-helper>
           <md-list-item md-virtual-repeat="item in vm" md-on-demand aria-label="Entry" class="row">
-
             <!-- DATE -->
             <div class="truncate-col date-col left">{{item.dateTime}}</div>
-
             <!-- TX ID -->
             <div class="truncate-col tx-col left" >
               <span>
                 <a target="_blank" href="https://live.blockcypher.com/btc/tx/{{item.txid}}">{{item.txid}}</a>
               </span>
             </div>
-
             <!-- FROM -->
             <div class="truncate-col info-col left">
-              <span ng-show = "item.to !== 'Multiple Inputs'">{{item.from}}</span>
-              <a ng-show = "item.to === 'Multiple Inputs'" ng-click="vm.jsonDetails($event, item.json)">{{item.from}}</a>
+             <span>{{item.from}}</span>
             </div>
-
             <!-- TO -->
             <div class="truncate-col info-col left">
               <span ng-show = "item.to !== 'Multiple Outputs'">{{item.to}}</span>
               <a ng-show = "item.to === 'Multiple Outputs'" ng-click="vm.jsonDetails($event, item.json)">{{item.to}}</a>
             </div>
-
             <!-- AMOUNT -->
             <div class="truncate-col amount-col left">
               <span>{{item.amount}}</span>
             </div>
-
             <!-- MESSAGE -->
             <div class="truncate-col message-col left">
               <span>{{item.displayMessage}}</span>
             </div>
-
             <!-- JSON -->
             <div class="truncate-col json-col">
               <a ng-click="vm.jsonDetails($event, item.json)">
                 <md-icon md-font-library="material-icons">code</md-icon>
               </a>
             </div>
-
           </md-list-item>
         </md-virtual-repeat-container>
       </md-list>
@@ -101,57 +84,78 @@ class VirtualRepeatBtcTransactionsComponent extends VirtualRepeatComponent {
     this.initializeVirtualRepeat(
       this.btcTransactionsProviderFactory.createProvider(this.account),
       /* decorator function */
-      (transaction: any | IBTCTransaction) => {
+      (transaction: any) => {
+        transaction.amount = transaction.vout[0].value;
         this.btcMessages.forEach(message => {
           if (message.txId == transaction.txid) {
             transaction.displayMessage = message.message;
-            if (transaction.displayMessage.length > 13) {
+            if(transaction.displayMessage.length > 13) {
               transaction.displayMessage = transaction.displayMessage.substr(0, 10).concat('...')
             }
             transaction.message = message.message
           }
         })
         transaction.dateTime = dateFormat(new Date(transaction.time * 1000), format);
-        if (transaction.vin.length > 1) {
-          transaction.from = "Multiple Inputs";
-        } else {
-          transaction.from = transaction.vin[0].addr;
-        }
-        if (transaction.vout.length > 2) {
-          transaction.to = 'Multiple Outputs'
-        } else {
-          let isSameChangeAddress = false;
-          let to;
-          let value;
-          transaction.vout.forEach(entry => {
-            if (entry.scriptPubKey && entry.scriptPubKey.addresses && transaction.from != 'Multiple Inputs' && entry.scriptPubKey.addresses.includes(transaction.from))
-              isSameChangeAddress = true;
-            else {
-              to = entry.scriptPubKey.addresses[0]
-              value = entry.value
-            }
-          });
-          if (!isSameChangeAddress){
-            transaction.to = 'Multiple Outputs'
-            transaction.amount = ''
-          }
-          else {
-            transaction.to = to
-            transaction.amount = value
+        transaction.from = transaction.vin[0].addr;
+        let totalInputs = 0;
+        let inputs = '';
+        for (let i = 0; i < transaction.vin.length; i++) {
+          totalInputs += parseFloat(transaction.vin[i].value);
+          inputs += `
+          ${transaction.vin[i].addr} (${transaction.vin[i].value})`;
+          if (transaction.vin[i].addr === this.account) {
+            transaction.from = transaction.vin[i].addr;
           }
         }
-        if(transaction.from == this.account)
+
+        let totalOutputs = 0;
+        let outputs = '';
+        for (let i = 0; i < transaction.vout.length; i++) {
+          totalOutputs += parseFloat(transaction.vout[i].value);
+          if (transaction.vout[i].scriptPubKey.addresses) {
+            outputs += `
+            ${transaction.vout[i].scriptPubKey.addresses[0]} (${transaction.vout[i].value})`;
+          }
+        }
+        // by default assign To field to zeroth address
+        for (let i = 0; i < transaction.vout.length && transaction.vout[i].scriptPubKey.addresses; i++) {
+          if (transaction.vout[i].scriptPubKey.addresses) {
+            transaction.to = transaction.vout[0].scriptPubKey.addresses[0];
+            break;
+          }
+        }
+        // if change address is same and API returns change address as zeroth address then point To field and volume to some other address
+        if (transaction.from === transaction.to) {
+          for (let i = 1; i < transaction.vout.length && transaction.vout[i].scriptPubKey.addresses; i++) {
+            transaction.to = transaction.vout[i].scriptPubKey.addresses[0];
+            transaction.amount = transaction.vout[i].value;
+            break;
+          }
+        }
+
+        // if BTC were transferred from the unlocked account address then show it as "-Amount"
+        if (inputs.includes(this.account)) {
           transaction.amount = `-${transaction.amount}`;
-        var inputs = '', outputs = ''
-        transaction.vin.forEach(entry => inputs = inputs.concat(`${entry.addr} (${entry.value}) \n`));
-        transaction.vout.forEach(entry => outputs = outputs.concat(`${entry.scriptPubKey.addresses[0]} (${entry.value}) \n`));
+        } else {
+          // if input does not include the current unlocked account address then output will always have it
+          for (let i = 0; i < transaction.vout.length; i++) {
+            if (transaction.vout[i].scriptPubKey.addresses && transaction.vout[i].scriptPubKey.addresses[0] === this.account) {
+              transaction.to = this.account;
+              transaction.amount = transaction.vout[i].value;
+            }
+          }
+        }
+        // if change address was different then show hardcoded output
+        if (!outputs.includes(this.account)) {
+          transaction.to = 'Multiple Outputs';
+        }
 
         transaction.json = {
           txid: transaction.txid,
           time: transaction.dateTime,
           block: transaction.blockheight,
-          totalInputs: transaction.valueIn,
-          totalOutputs: transaction.valueOut,
+          totalInputs,
+          totalOutputs,
           confirmations: transaction.confirmations,
           fees: transaction.fees,
           inputs: inputs.trim(),
@@ -197,8 +201,4 @@ class VirtualRepeatBtcTransactionsComponent extends VirtualRepeatComponent {
       })
     }
   }
-}
-
-interface IBTCTransaction {
-
 }
