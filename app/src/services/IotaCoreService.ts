61a13d532bc676fd1fad7244dd3e01f301c7a72c
Abhiram <ahatolkar-grepruby@grepruby.com>
2019-07-12 13:30:59 +0530

    Show first zero balance address of iota

--- app/src/services/IotaCoreService.ts
+++ app/src/services/IotaCoreService.ts
@@ -5,7 +5,7 @@ class IotaCoreService {
   private iotaCore;
 
   constructor($window: angular.IWindowService) {
-              this.iotaCore = $window.heatlibs.IotaCore;
+    this.iotaCore = $window.heatlibs.IotaCore;
   }
 
   /* Sets the 12 word seed to this wallet, note that seeds have to be bip44 compatible */
@@ -25,8 +25,8 @@ class IotaCoreService {
     return walletType;
   }
 
-  getWallet(mnemonic: string, index: Number = 0, securityLevel: number = 2) {
-    const address = this.iotaCore.generateAddress(mnemonic, 0, 2, false)
+  getWallet(mnemonic: string, index: Number = 0, securityLevel: number = 2): any {
+    const address = this.iotaCore.generateAddress(mnemonic, index, securityLevel, false)
     return {
       address,
       privateKey: mnemonic
@@ -36,16 +36,32 @@ class IotaCoreService {
 
   refreshAdressBalances(wallet: WalletType) {
     return new Promise((resolve, reject) => {
+      let secretPhrase = wallet.addresses[0].privateKey;
       let iotaBlockExplorerService: IotaBlockExplorerService = heat.$inject.get('iotaBlockExplorerService')
       iotaBlockExplorerService.getInputs(wallet.addresses[0].privateKey).then(info => {
-        info.inputs.forEach(input => {
-          let walletAddress: any = {};
-          walletAddress.inUse = true;
-          walletAddress.balance = input.balance + ""
-          walletAddress.address = input.address
-          walletAddress.privateKey = wallet.addresses[0].privateKey // keep same private key (should be treated as seed) until iota offline signing is done
-          wallet.addresses.push(walletAddress)
-      })
+        if (info.inputs !== []) {
+          let index = 0;
+          wallet.addresses = [];
+          info.inputs.forEach(input => {
+            let walletAddress: any = {};
+            walletAddress.inUse = true;
+            walletAddress.balance = input.balance + ""
+            walletAddress.address = input.address
+            walletAddress.privateKey = secretPhrase // keep same private key (should be treated as seed) until iota offline signing is done
+            index = input.keyIndex;
+            wallet.addresses.push(walletAddress)
+          })
+          let zeroBalanceAccount = this.getWallet(secretPhrase, ++index)
+          zeroBalanceAccount.inUse = true;
+          zeroBalanceAccount.balance = "0"
+          wallet.addresses.push(zeroBalanceAccount)
+        }
+        else {
+          let zeroBalanceAccount = this.getWallet(secretPhrase)
+          zeroBalanceAccount.inUse = true;
+          zeroBalanceAccount.balance = "0"
+          wallet.addresses.push(zeroBalanceAccount)
+        }
         resolve(true)
       }, () => {
         resolve(false)
