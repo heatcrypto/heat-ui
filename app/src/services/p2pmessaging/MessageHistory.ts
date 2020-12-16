5bf6f5a2441405eed64536f23a13ad6c3d5a43ad
resultingforce <alexey.medvedev@gmail.com>
2020-12-16 20:06:02 +0300

    Fix shrinking local storage on saving messages page

--- app/src/services/p2pmessaging/MessageHistory.ts
+++ app/src/services/p2pmessaging/MessageHistory.ts
@@ -290,8 +290,8 @@ module p2p {
 
     private savePage(pageIndex: number, pageContent: Array<MessageHistoryItem>) {
       let occupiedSpaceBefore = this.checkStorageSpace(true, false)
-      let occupiedSpaceAfter = this.checkStorageSpace(false, true)
-      if (occupiedSpaceAfter != occupiedSpaceBefore) {
+      let occupiedSpaceAfter = occupiedSpaceBefore ? this.checkStorageSpace(false, true) : null
+      if (occupiedSpaceAfter && occupiedSpaceAfter != occupiedSpaceBefore) {
         console.log(`Removed data length ${occupiedSpaceBefore - occupiedSpaceAfter}, storage occupied space ${occupiedSpaceAfter}`)
       }
 
